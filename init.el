a8f197c9137c240547b25c36a2010cc1124dbf01
Mattias Sattel <matthias.sattel@cgm.com>
2016-01-30 01:23:56 +0100

    Basic settings

---
+++ init.el
@@ -0,0 +1,18 @@
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+(setq settings-dir
+      (expand-file-name "settings" user-emacs-directory))
+
+;; Set up load path
+(add-to-list 'load-path settings-dir)
+
+;; Set up appearance early
+(require 'appearance)
+
+(require 'customization)
