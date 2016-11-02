5e41237dc632437885ee71c5cd4df4f23d77179a
Mattias Sattel <matthias.sattel@cgm.com>
2016-11-02 18:17:14 +0100

    customizations

---
+++ settings/customization.el
@@ -0,0 +1,95 @@
+(add-to-list 'load-path "/usr/share/emacs/site-lisp")
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango)))
+ '(custom-safe-themes
+   (quote
+    ("9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;(require 'package)
+;(add-to-list 'package-archives
+; '("marmalade" .
+;      "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives
+;             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+;  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;(when (>= emacs-major-version 24)
+;  (add-to-list
+;   'package-archives
+;   '("melpa-stable" . "http://stable.melpa.org/packages/")
+;   t))
+;(package-initialize)
+
+;(require 'ido)
+;(ido-mode t)
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;arduino
+(add-to-list 'load-path "~/.emacs.d/vendor/arduino-mode")
+(setq auto-mode-alist (cons '("\\.\\(pde\\|ino\\)$" . arduino-mode) auto-mode-alist))
+(autoload 'arduino-mode "arduino-mode" "Arduino editing mode." t)
+
+;org-mode
+(add-to-list 'load-path "~/.emacs.d/vendor/org-mode/lisp")
+(require 'org)
+;(define-key global-map "\C-cl" 'org-store-link)
+;(define-key global-map "\C-ca" 'org-agenda)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-log-done t)
+
+;org-babel
+(org-babel-do-load-languages
+      'org-babel-load-languages
+      '((emacs-lisp . t)
+	(java . t)
+	(clojure . t)))
+
+;clojure
+;(unless (package-installed-p 'cider)
+;  (package-install 'cider))
+;(add-hook 'cider-mode-hook #'eldoc-mode)
+;(add-hook 'cider-repl-mode-hook #'company-mode)
+;(add-hook 'cider-mode-hook #'company-mode)
+(eval-after-load 'clojure-mode
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c C-h") #'clojure-cheatsheet)))
+(load-file "~/.emacs.d/cider.el")
+
+;company mode enabled globally
+(add-hook 'after-init-hook 'global-company-mode)
+
+;markdown
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(provide 'customization)
