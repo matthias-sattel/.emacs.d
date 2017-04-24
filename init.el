4fc81e437af7caacec7936136712c1c577d4da09
Mattias Sattel <matthias.sattel@cgm.com>
2017-04-24 18:59:11 +0200

    C-n will now create a new line at the end of the buffer

--- init.el
+++ init.el
@@ -153,9 +153,4 @@
 ;appearance
 (require 'appearance)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq next-line-add-newlines t)
